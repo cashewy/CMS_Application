9598233ef05c6cb33bd5c01ad8ba7fe99d3d7db8
cashewy <37968922+cashewy@users.noreply.github.com>
2020-10-14 20:56:17 -0400

    Added file logging with seri log and test case in program file

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LoggingService;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CMS_Application
 {
@@ -13,13 +18,43 @@ namespace CMS_Application
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var currentHost = CreateHostBuilder(args).Build();
+            using (var scope = currentHost.Services.CreateScope())
+            {
+                try
+                {
+                    int zero = 0;
+                    int result = 100 / zero;
+
+
+                }
+                catch(DivideByZeroException ex)
+                {
+                    Log.Error("An error occurred while seeding the database  {Error} {StackTrace} {InnerException} {Source}",
+                                         ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+                }
+            }
+
+            currentHost.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                     .Enrich.FromLogContext()
+                     .Enrich.WithProperty("Application", "CMS_CORE_NG")
+                     .Enrich.WithProperty("MachineName", Environment.MachineName)
+                     .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                     .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+                     .Enrich.WithProperty("Version", Environment.Version)
+                     .Enrich.WithProperty("UserName", Environment.UserName)
+                     .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+                     .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                     .WriteTo.File(formatter: new CustomerTextFormater(), 
+                     path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath + $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}", $"cms_core_ng_{DateTime.Now:yyyyMMdd}.txt"))
+                    .ReadFrom.Configuration(hostingContext.Configuration));
                     webBuilder.UseStartup<Startup>();
                 });
     }
